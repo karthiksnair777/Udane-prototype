e7327d8470d86edc3f3dbbd2f01a85ae160ed3b9
Sujith Balan <sujithbalan2001@gmail.com>
2025-09-11 13:32:16 +0530

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,9 +13,12 @@ import POSManageProducts from './pos/POSManageProducts';
 
 // Customer imports
 import CustomerLogin from './customer/CustomerLogin';
+import CustomerFront from './customer/CustomerFront';
 // import ShopSelection from './customer/ShopSelection';
+import CustomerHome from './customer/CustomerHome';
 import CustomerProducts from './customer/CustomerProducts';
 import CustomerCart from './customer/CustomerCart';
+import CustomerCheckout from './customer/CustomerCheckout';
 import CustomerOrders from './customer/CustomerOrders';
 
 
@@ -33,11 +36,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path="/pos/manage-products" element={<POSManageProducts />} />
       <Route path="*" element={<div>Page not found</div>} />
 
-       <Route path="/customer/login" element={<CustomerLogin />} />
-      {/* <Route path="/customer/shops" element={<ShopSelection />} /> */}
-      <Route path="/customer/products" element={<CustomerProducts />} />
-      <Route path="/customer/cart" element={<CustomerCart />} />
-      <Route path="/customer/orders" element={<CustomerOrders />} />
-    </Routes>
-  </BrowserRouter>
+        <Route path="/customer/login" element={<CustomerLogin />} />
+        <Route path="/customer" element={<CustomerFront />} />
+        {/* <Route path="/customer/shops" element={<ShopSelection />} /> */}
+        <Route path="/customer/home" element={<CustomerHome />} />
+        <Route path="/customer/products" element={<CustomerProducts />} />
+        <Route path="/customer/cart" element={<CustomerCart />} />
+        <Route path="/customer/checkout" element={<CustomerCheckout />} />
+        <Route path="/customer/orders" element={<CustomerOrders />} />
+      </Routes>
+    </BrowserRouter>
+ 
 );
